9d4a9ceb6eb64c894187910a4672bb13d9884f93
Lohana <Loha@hotmail.com>
2023-04-30 21:04:23 +0200

    componente figura con renderizado condicional usando eventos de mouse

--- src/Componets/pure/Figura.jsx
+++ src/Componets/pure/Figura.jsx
@@ -1,21 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Figura = () => {
 
+const [fondo, setFondo] = useState('#000000');
 
-const contenedor={
+const [ManageIntervalo, setManageIntervalo] = useState(0);
 
-    width:'255px' ,
-    height:'255px',
-    backgroundColor:'black',
-    margin:'2rem',
+const [DobleClick, setDobleClick] = useState(0);
+
+const getFondo = () => Math.floor(Math.random() * 256);
+
+
+
+const getHex = (red, green, blue) => {
+  return (
+    '#' +
+    [red, green, blue]
+      .map((c) => {
+        const hex = c.toString(16);
+        return hex.lenght === 1 ? '0' + hex : hex;
+      })
+      .join('')
+  );
+};
+
+const generateHex = () => {
+  const rgb = {
+    r: getFondo(),
+    g: getFondo(),
+    b: getFondo(),
+  };
+  return setFondo(getHex(rgb.r, rgb.g, rgb.b));
+};
+
+const CambioFondo = () => {
+    return setManageIntervalo(setInterval(generateHex,500));
+};
+
+const StopCambioColor= () =>{
+    clearInterval(ManageIntervalo);
+    
 }
+
+const CambioClick= () =>{
+    clearInterval(ManageIntervalo);
+}
+
+
 
   return (
     <div>
-    <div style={contenedor}></div>
+    <div
+     onMouseOver={CambioFondo}
+         onMouseLeave={StopCambioColor}
+         onDoubleClick={CambioClick}
+        style={{width:'255px', height:'255px',backgroundColor: fondo, margin:'auto'}}></div>
+        {' '}
+      <p style={{ color: 'black' }}> Color: {fondo} </p>
     </div>
   )
-}
 
+  }
 export default Figura
