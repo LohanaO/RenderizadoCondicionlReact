d3eef428eaa3e46e0c2acdefedd76a860efea587
Lohana <Loha@hotmail.com>
2023-04-30 20:24:52 +0200

    Renderizado condicional de un componente

---
+++ src/Componets/pure/Figura.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Figura = () => {
+
+
+const contenedor={
+
+    width:'255px' ,
+    height:'255px',
+    backgroundColor:'black',
+    margin:'2rem',
+}
+
+  return (
+    <div>
+    <div style={contenedor}></div>
+    </div>
+  )
+}
+
+export default Figura
